5431e57f93ff678bb9befec35d0961efa3cb4692
zhengwu <rocky.zheng314@gmail.com>
2025-12-03 20:08:24 +0800

    examplre:brute force solver

---
+++ sudoku-rs/examples/brute_force.rs
@@ -0,0 +1,9 @@
+use sudoku_rs::{grid::Grid, solver::brute_force};
+
+fn main() {
+    let s = ".3.2.71.6..9.3...8.6..8............9.961.853.8............1..8.9...5.7..2.56.3.1.";
+    let grid = Grid::new_from_singline_digit(s).unwrap();
+    let solver = brute_force::BruteForceSolver::new();
+    let solution_grid = solver.solve(grid).unwrap();
+    println!("{}", solution_grid.to_digit_line());
+}
