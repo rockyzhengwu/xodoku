c61911ea5b7fc1c292ca9a90f9edfecbad3f424b
zhengwu <rocky.zheng314@gmail.com>
2025-12-05 22:03:07 +0800

    example: update bruteforce solver

--- sudoku-rs/examples/brute_force.rs
+++ sudoku-rs/examples/brute_force.rs
@@ -4,6 +4,6 @@ fn main() {
     let s = ".3.2.71.6..9.3...8.6..8............9.961.853.8............1..8.9...5.7..2.56.3.1.";
     let grid = Grid::new_from_singline_digit(s).unwrap();
     let solver = brute_force::BruteForceSolver::new();
-    let solution_grid = solver.solve(grid).unwrap();
-    println!("{}", solution_grid.to_digit_line());
+    let solution = solver.solve(grid);
+    println!("{:?}", solution.values());
 }
